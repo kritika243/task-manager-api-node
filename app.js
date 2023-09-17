21bf95264d688d7ff342f60e11b035b4721d6310
kritika243 <24kritikasharma24@gmail.com>
2023-09-17 13:00:57 +0530

    basic start

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Hello World')
+const express = require('express')
+const app = express()
+
+// routes
+app.get('/', (req, res) => {
+  res.send('Welcome')
+})
+
+const port = 5001
+
+app.listen(port, () => {
+  console.log(`server is listening on ${port}`)
+})
+
+
