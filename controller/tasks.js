94c24b3f8f880e9dcb3db9200ebe290623409ed7
kritika243 <24kritikasharma24@gmail.com>
2023-09-17 14:39:47 +0530

    setting up all routes

--- controller/tasks.js
+++ controller/tasks.js
@@ -1,10 +1,27 @@
 
 
 const getAllTasks = (req, res) => {
-  res.send('all items')
+  res.send('get all tasks')
+}
+
+const createNewTask = (req, res) => {
+  res.send('create new task')
+
+}
+const getTask = (req, res) => {
+  res.send('get single task')
+
+}
+const updateTask = (req, res) => {
+  res.send('update task')
+
+}
+const deleteTask = (req, res) => {
+  res.send('delete task')
+
 }
 
 
 module.exports = {
-  getAllTasks
+  getAllTasks, createNewTask, getTask, updateTask, deleteTask
 }
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const { getAllTasks } = require('../controller/tasks')
+const { getAllTasks, createNewTask, getTask, updateTask, deleteTask } = require('../controller/tasks')
 const router = express.Router()
 
 
-router.route('/').get(getAllTasks)
+router.route('/').get(getAllTasks).post(createNewTask)
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
 
 
 
