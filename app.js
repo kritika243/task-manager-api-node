bd2b5a7a7c342df42e738a36199ac435a7f527dc
kritika243 <24kritikasharma24@gmail.com>
2023-09-22 12:51:08 +0530

    mongo db and env initial setup

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
 const taskRoutes = require('./routes/tasks')
+const connectDB = require('./db/connect')
 
 
 // middleware
@@ -10,8 +12,18 @@ app.use('/api/v1/tasks', taskRoutes)
 
 const port = 5001
 
-app.listen(port, () => {
-  console.log(`server is listening on ${port}`)
-})
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_CONNECTION_STRING)
+    app.listen(port, () => {
+      console.log(`server is listening on ${port}`)
+    })
 
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+start()
 
