bfac944d0c33b0f3256038ed1c7013433c15791a
kritika243 <24kritikasharma24@gmail.com>
2023-10-01 22:27:12 +0530

    get all task and get single task endpoint handling

--- controller/tasks.js
+++ controller/tasks.js
@@ -1,7 +1,12 @@
 const Task = require('../models/Task')
 
-const getAllTasks = (req, res) => {
-  res.send('get all tasks')
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({})
+    res.status(200).json({ tasks })
+  } catch (error) {
+    res.status(500).json({ message: error })
+  }
 }
 
 const createNewTask = async (req, res) => {
@@ -13,8 +18,18 @@ const createNewTask = async (req, res) => {
   }
 
 }
-const getTask = (req, res) => {
-  res.json({ id: req.params.id })
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params
+    const task = await Task.findOne({ _id: taskID })
+    if (!task) {
+      return res.status(404).json({ message: `No task for the ${taskID} ` })
+    }
+    res.status(200).json({ task })
+  } catch (error) {
+    res.status(500).json({ message: error })
+  }
+
 
 }
 const updateTask = (req, res) => {
