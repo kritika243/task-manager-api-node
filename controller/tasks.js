b25d26c711d840efed9ca6f80c37da311599ed44
kritika243 <24kritikasharma24@gmail.com>
2023-10-03 11:23:37 +0530

    delete api

--- controller/tasks.js
+++ controller/tasks.js
@@ -36,7 +36,17 @@ const updateTask = (req, res) => {
   res.send('update task')
 
 }
-const deleteTask = (req, res) => {
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params
+    const task = await Task.findOneAndDelete({ _id: taskID })
+    if (!task) {
+      return res.status(404).json({ message: `No task for the ${taskID} ` })
+    }
+    res.status(200).json({ task })
+  } catch (error) {
+    res.status(500).json({ message: error })
+  }
   res.send('delete task')
 
 }
